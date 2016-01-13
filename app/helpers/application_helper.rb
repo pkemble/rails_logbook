1cf8c8cc28be6ddb0c447617a61c601ee8ef8435
pkemble <pkemble@gmail.com>
2016-01-13 09:05:28 -0500

    Add static pages / tripsheet first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def on_trip_sheet_page
+    current_page?('/static_pages/tripsheet.html')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/tripsheet'
+
 #  resources :entries
   resources :flights
   get 'logbook/index'
