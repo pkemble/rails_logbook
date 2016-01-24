6224d763e55f4b24eed139b6fae1832d44f9e933
pkemble <pkemble@gmail.com>
2016-01-23 19:14:18 -0500

    added other files

---
+++ lib/hobbstime.rb
@@ -0,0 +1,32 @@
+#Times Util
+
+class HobbsTime
+  
+  attr_reader :hobbs_start, :hobbs_end, :hobbs_date, :span
+  
+  HMFormat = "%H%M"
+  
+  def initialize(t1=nil, t2=nil, date=Date.today) # TODO make date arg required.
+    @hobbs_start = t1.nil? ? date.beginning_of_day : Time.strptime(t1, HMFormat)
+    @hobbs_end = t2.nil? ? date.tomorrow : Time.strptime(t2, HMFormat)
+    @hobbs_date = date
+    @span = span
+  end
+
+  def span
+    # if @hobbs_start.nil? && @hobbs_end.nil?
+      # return 24
+    # end
+        
+    if @hobbs_end < @hobbs_start
+      @hobbs_start = @hobbs_start.prev_day
+    end
+    
+    return ((@hobbs_end - @hobbs_start) / 3600 ).round(1)
+  end
+  
+  # gets a time object and returns the HMFormat constant format 
+  def self.to_short_format(t)
+    return t.localtime.strftime(HMFormat) #TODO revisit timezones
+  end
+end
