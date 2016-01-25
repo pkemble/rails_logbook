95e51027c1d630e7fc188bd509425ff25e4fcb52
pkemble <pkemble@gmail.com>
2016-01-25 15:04:08 -0500

    added user model - started adding bootstrap stuff. switching to a bootstrap branch for the rest of that.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119190506) do
+ActiveRecord::Schema.define(version: 20160124154846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 20160119190506) do
   create_table "import_exports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "uname"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "flights", "entries"
