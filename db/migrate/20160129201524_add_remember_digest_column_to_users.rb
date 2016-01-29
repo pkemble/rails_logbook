0d87f8051b7c8e1f673e7a9604d160ddfeb9abf7
pkemble <pkemble@gmail.com>
2016-01-29 15:24:14 -0500

    more user stuff

---
+++ db/migrate/20160129201524_add_remember_digest_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddRememberDigestColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :remember_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124154846) do
+ActiveRecord::Schema.define(version: 20160129201524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160124154846) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
   add_foreign_key "flights", "entries"
