bde95a26eec6e54b61577c023e02dfe927e28a20
pkemble <pkemble@gmail.com>
2016-01-29 21:03:00 -0500

    more user stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	attr_accessor :remember_token
 	before_save { email.downcase! }
 	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :uname, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
 	validates :email, :presence => true, uniqueness: { case_sensitive: false }, :format => EMAIL_REGEX
 	validates :password, :confirmation => true
 	validates_length_of :password, :in => 6..20, :on => :create
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129201524) do
+ActiveRecord::Schema.define(version: 20160130015459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20160129201524) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
-    t.string   "uname"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
