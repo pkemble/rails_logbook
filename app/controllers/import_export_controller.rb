2a2a898c2d1d7e7fdee230380f0a6e1697389ee9
pkemble <pkemble@gmail.com>
2016-02-05 12:19:41 -0500

    more io work

--- app/controllers/import_export_controller.rb
+++ app/controllers/import_export_controller.rb
@@ -1,12 +1,18 @@
 class ImportExportController < ApplicationController
+  
+  before_action :logged_in_user
+  before_action :correct_user
+  
+  
   def index
-    @month = Time.now.prev_month.strftime('%B')
+    @prev_month = Time.now.prev_month.strftime('%B')
   end
   
   def psi_expense
+    @user = current_user
     @t = Time.now.prev_month.beginning_of_month
     @e = Time.now.prev_month.end_of_month
-    @entries = Entry.where(date: @t..@e).order(:date)
+    @entries = Entry.where(date: @t..@e, user_id: @user.id ).order(:date)
     #render :text => psi_output TODO
     
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name: "Seedy Kemble", email: "pkemble_seed@gmail.com", password: "zxcvbnm,.", password_confirmation: "zxcvbnm,.")
+
+50.times do |n|
+  date = rand(Date.civil(2013, 8, 5)..Date.civil(2016, 2, 1))
+  tail = "N656AF"
+  per_diem_hours = 24
+  
+  Entry.create( date: date,
+                tail: tail,
+                per_diem_hours: per_diem_hours,
+                user: User.find_by(email: "pkemble_seed@gmail.com") )
+end
