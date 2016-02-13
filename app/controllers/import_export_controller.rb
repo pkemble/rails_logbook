669e2c45c6daa4a4edcb141c46214ce1e554ef3a
pkemble <pkemble@gmail.com>
2016-02-13 14:35:15 -0500

    master needs some of this...

--- app/controllers/import_export_controller.rb
+++ app/controllers/import_export_controller.rb
@@ -6,14 +6,19 @@ class ImportExportController < ApplicationController
   
   def index
     @prev_month = Time.now.prev_month.strftime('%B')
+    @past_months = Entry.past_months
   end
   
   def psi_expense
     @user = current_user
-    @t = Time.now.prev_month.beginning_of_month
-    @e = Time.now.prev_month.end_of_month
-    @entries = Entry.where(date: @t..@e, user_id: @user.id ).order(:date)
-    #render :text => psi_output TODO
-    
+    if params[:month].nil? 
+      @t = Time.now.prev_month.beginning_of_month
+      @e = Time.now.prev_month.end_of_month
+      @entries = Entry.where(date: @t..@e, user_id: @user.id ).order(:date)
+      #render :text => psi_output TODO
+    else
+      
+    end
+      
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'logbook/index'
   
   get 'io' => 'import_export#index'
-  get 'psi_expense_report' => 'import_export#psi_expense'
+  get 'psi_expense_report/:month' => 'import_export#psi_expense'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
